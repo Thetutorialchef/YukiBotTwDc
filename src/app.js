0411ab76b1fb5f7215b5f3d6f9f4118e1a52abcd
NullDev (Chris) <shadow@nulldev.org>
2020-03-09 23:34:25 +0100

    pruned main entry file

--- src/app.js
+++ src/app.js
@@ -1,25 +1,19 @@
 "use strict";
+
+// Dependencies
 let Discord = require("discord.js");
 
-let conf = require("./handler/configHandler");
-let log = require("./util/logger");
-
-
-
-
+// Utils
+let conf = require("./utils/configParser");
+let log = require("./utils/logger");
 
 let messageHandler = require("./handler/msgHandler");
-//let reactionHandler = require("./handler/reactionHandler");
-
-
 
 let version = conf.getVersion();
 let appname = conf.getName();
 let devname = conf.getAuthor();
 
 let splashPadding = 12 + appname.length + version.toString().length;
-
-
 
 console.log(
     "\n" +
@@ -34,41 +28,12 @@ log.done("Started.");
 const config = conf.getConfig();
 const client = new Discord.Client();
 
-//Twitch Connection
-
-let TwitchWebHookListener = require('twitch-webhooks').default;
-let TwitchClient = require('twitch').default;
-let twitchClient = TwitchClient.withCredentials(config.twitch.twitch_client_id, config.twitch.twitch_client_secret);
-
-let streamer = "The_Tutorial_Chef"
-
-async function isStreamLive(userName) {
-	const user = await twitchClient.helix.users.getUserByName(userName);
-	if (!user) {
-		return false;
-	}
-	return await twitchClient.helix.streams.getStreamByUserId(user.id) !== null;
-}
-
-//let live = isStreamLive("The_Tutorial_Chef");
-
-
-
-
 process.on("unhandledRejection", (err, promise) => log.error(`Unhandled rejection (promise: ${promise}, reason: ${err})`));
 
 client.on("ready", () => {
     log.info("Running...");
     log.info(`Got ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds`);
     client.user.setActivity(config.bot_settings.status);
-
-
-    if(isStreamLive("The_Tutorial_Chef")){
-
-
-
-    }
-
 });
 
 client.on("guildCreate", guild => log.info(`New guild joined: ${guild.name} (id: ${guild.id}) with ${guild.memberCount} members`));
@@ -79,8 +44,6 @@ client.on("message", (message) => messageHandler(message, client));
 
 client.on("error", log.error);
 
-//client.on("raw", async event => reactionHandler(event, client));
-
 client.login(config.auth.bot_token).then(() => {
     log.done("Token login was successful!");
 }, (err) => {
@@ -88,6 +51,3 @@ client.login(config.auth.bot_token).then(() => {
     log.error("Shutting down due to incorrect token...\n\n");
     process.exit(1);
 });
-
-
-
